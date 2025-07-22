554fc47c758faf98104498137075e340bcff09ca
Cortejua <j.cortesp06@gmail.com>
2025-07-22 16:32:14 -0500

    Primer commit del backend

---
+++ src/main/java/com/netkrow/backend/dto/QueryRequest.java
@@ -0,0 +1,12 @@
+package com.netkrow.backend.dto;
+
+public class QueryRequest {
+    private String sql;
+
+    public String getSql() {
+        return sql;
+    }
+    public void setSql(String sql) {
+        this.sql = sql;
+    }
+}
